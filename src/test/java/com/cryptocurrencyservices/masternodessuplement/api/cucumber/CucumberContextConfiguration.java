fa18d9b4baee846b4a0c06a11e1624bfcef4b955
kevin <kjwilde@hotmail.com>
2019-04-15 15:59:22 -0500

    Initial import

---
+++ src/test/java/com/cryptocurrencyservices/masternodessuplement/api/cucumber/CucumberContextConfiguration.java
@@ -0,0 +1,20 @@
+package com.cryptocurrencyservices.masternodessuplement.api.cucumber;
+
+import com.cryptocurrencyservices.masternodessuplement.api.MasternodesOnlineSupplementeApiApp;
+import cucumber.api.java.Before;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.web.WebAppConfiguration;
+
+@SpringBootTest
+@WebAppConfiguration
+@ContextConfiguration(classes = MasternodesOnlineSupplementeApiApp.class)
+public class CucumberContextConfiguration {
+
+    @Before
+    public void setup_cucumber_spring_context(){
+        // Dummy method so cucumber will recognize this class as glue
+        // and use its context configuration.
+    }
+
+}
